742462a4aec6af2cc0e452cf368c4b34d39c0920
Panu <panusalonen@gmail.com>
2020-03-09 10:40:19 +0100

    final

--- src/components/App.js
+++ src/components/App.js
@@ -8,16 +8,22 @@ class App extends React.Component {
     state = {
         videos: [],
         selectedVideo: null
-    };
+    }
+    componentDidMount(){
+        this.onTermSubmit('buildings')
+    }
     onTermSubmit = async term => {
         const response = await youtube.get('/search', {
             params: {
                 q: term
             }
-        });
+        })
         console.log(response)
-        this.setState({ videos: response.data.items })
-    };
+        this.setState({ 
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        })
+    }
     onVideoSelect = video => {
         this.setState({ selectedVideo: video })
     }
